326e6de0b62f538e272504055f5e334a2a5da064
Akshendra Pratap <akshendra521994@gmail.com>
2018-03-09 17:55:59 +0530

    export things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 const asg = require('./libs/asg');
+const cw = require('./libs/cw');
+const ecs = require('./libs/ecs');
+const lb = require('./libs/lb');
+const sns = require('./libs/sns');
 
 module.exports = {
-  asg,
+  asg, cw, ecs, lb, sns,
 };
