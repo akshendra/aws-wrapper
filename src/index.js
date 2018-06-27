ccff237ca135bd4a7cac8733da63bd3bc0c9a4ce
Akshendra Pratap <akshendra521994@gmail.com>
2018-06-27 17:10:59 +0530

    add more services

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const cw = require('./libs/cw');
 const ecs = require('./libs/ecs');
 const lb = require('./libs/lb');
 const sns = require('./libs/sns');
+const s3 = require('./libs/s3');
+const codepipeline = require('./libs/codepipeline');
 
 module.exports = {
-  asg, cw, ecs, lb, sns,
+  asg, cw, ecs, lb, sns, codepipeline, s3,
 };
